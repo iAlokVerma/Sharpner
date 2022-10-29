50296249ecaea032a56be327aa7ff5e0349c821b
alokkumarv <alok13080@gmail.com>
2022-10-29 22:30:47 +0530

    changes color bold

--- main.js
+++ main.js
@@ -1,9 +1,23 @@
-third_element = document.querySelectorAll("li")[2]
-third_element.style.background = "green"
+ul = document.getElementById('items')
 
+new_li = document.createElement("li")
+new_li.className = "list-group-item"
+new_li.innerText = "New Element"
+ul.append(new_li)
+console.log(ul)
 
-li = document.querySelectorAll("li")
-for(var ele=0;ele<li.length;ele++){
-    li[ele].style.fontWeight = "bold"
-    li[ele].style.color = "red"
+li = document.getElementsByTagName("li")
+
+for(var i=0 ;i<li.length;i++){
+
+li[i].style.background = "pink"
+
+}
+
+li = document.getElementsByClassName("list-group-item")
+console.log(li)
+for(var i=0 ;i<li.length;i++){
+
+li[i].style.color = "red"
+
 }
