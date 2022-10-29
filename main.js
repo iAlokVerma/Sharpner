34e576aff9577ab50dc128e90763f530d7547f97
alokkumarv <alok13080@gmail.com>
2022-10-29 22:07:30 +0530

    changed

---
+++ main.js
@@ -0,0 +1,6 @@
+title = document.getElementById("header-title")
+add_item = document.getElementById("add_item")
+title.style.border = "solid 2px black"
+console.log(add_item)
+add_item.style.color = "green"
+add_item.style.fontWeight = "bold"
