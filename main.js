928dc560d6e0db770f52dcf27367d37dd3e16a14
alokkumarv <alok13080@gmail.com>
2022-10-30 22:47:01 +0530

    dom manupulation

--- main.js
+++ main.js
@@ -1,20 +1,33 @@
-container = document.querySelector(" .container")
-console.log(container)
-child = document.querySelector("#header-title")
-text = document.createTextNode("Hello World")
-li = document.createElement('h1')
-li.append(text)
-// console.log(li,container)
-container.insertBefore(li,child)
+let form  = document.getElementById("addForm")
+let inputText =document.getElementById("item")
+let ul = document.getElementById("items")
 
 
-
-items = document.querySelector('#items')
-
+ul.addEventListener("click",removeelement)
 
 
-text = document.createTextNode("Hello World")
-li = document.createElement('li')
-li.className = 'list-group-item'
-li.append(text)
-items.insertBefore(li,items.firstElementChild)
+function removeelement(e) {
+    if (e.target.classList.contains('delete')){
+        let li = e.target.parentElement
+        ul.removeChild(li)
+    }
+    
+}
+
+
+form.addEventListener('submit',addItem)
+
+function addItem(event){
+    event.preventDefault()
+    let item  = inputText.value
+    let button = document.createElement('button')
+    button.className = "btn btn-danger btn-sm float-right delete"
+    button.innerText = "x"
+    let newList =  document.createElement('li')
+    newList.className = "list-group-item"
+    newList.innerText = item
+    newList.appendChild(button)
+    ul.appendChild(newList)
+
+    
+}
