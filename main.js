7f05c41f5a31a9cda93b9899d2d0b3f7d6fc14ff
alokkumarv <alok13080@gmail.com>
2022-11-01 23:11:34 +0530

    dom manupulation

--- main.js
+++ main.js
@@ -1,74 +1,18 @@
-editItem = document.getElementById("item")
-editDescription = document.getElementById("description")
-form = document.getElementById("addForm")
-itemlist = document.getElementById("items")
+let inusername = document.querySelector("#name")
+let inemail = document.querySelector("#email")
+btnsubmit = document.querySelector(".btn")
+btnsubmit.addEventListener("click",saveuser)
 
-form.addEventListener("submit",addItem)
-itemlist.addEventListener("click",removeListItem)
-
-filter = document.getElementById("filter")
-filter.addEventListener("keyup",filterItem)
-
-
-
-function filterItem(e) {
-    console.log("e")
-    searchfor = e.target.value.toLowerCase()
-    itemsList = document.getElementsByClassName("i")
-    Array.from(itemsList).forEach((div)=>{
-        if ((div.children[0].innerText.toLowerCase().indexOf(searchfor) != -1) || (div.children[1].innerText.toLowerCase().indexOf(searchfor) != -1)){
-                div.style.display = "block"
-        }
-        else{
-                div.style.display = "none"
-        }
-        
-    })
-    
-}
-
-
-function removeListItem(e) {
-    if (e.target.classList.contains("delete")){
-        li = e.target.parentElement.parentElement
-        console.log(li)
-        li.removeChild(e.target.parentElement)
-    }
-}
-
-function addItem(e) {
+function saveuser(e) {
     e.preventDefault()
-    item = editItem.value
-    descrption = editDescription.value
+    if (inusername.value != "" ||  inemail.value != ""){
+        let username = inusername.value
+        let email = inemail.value
+        console.log(username)
+        console.log(email)
+        save = `user : ${username } email : ${email}`
+        console.log(save)
+        localStorage.setItem("user",`user : ${username} email : ${email}`)
+    }
     
-    newitem = document.createElement("li")
-    newitem.className = "list-group-item"
-    newitem.appendChild(document.createTextNode(item))
-
-    newdescrption = document.createElement("li")
-    newdescrption.className = "list-group-item"
-    newdescrption.appendChild(document.createTextNode(descrption))
-
-
-    newButton = document.createElement("button")
-    newButton.className = "btn btn-danger btn-sm float-right delete"
-    newButton.appendChild(document.createTextNode("X"))
-
-    neweditButton = document.createElement("button")
-    neweditButton.className = "btn btn-danger btn-sm float-right mr-4 delete"
-    neweditButton.appendChild(document.createTextNode("Edit"))
-
-
-    div = document.createElement("div")
-    div.className = "i"
-    console.log(item)
-    console.log(newdescrption)
-    div.appendChild(newitem)
-    div.appendChild(newdescrption)
-    div.appendChild(newButton)
-    div.appendChild(neweditButton)
-
-    itemlist.appendChild(div)
-
-
 }
