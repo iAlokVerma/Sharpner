77090b980dbab1d82cdb5ad94ef1f14e68a827f9
alokkumarv <alok13080@gmail.com>
2022-11-22 23:29:34 +0530

    added delete feature

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-BASE_URL = "https://crudcrud.com/api/0c490d692bf846acb5b2d4bb39bf1674/appointments"
+BASE_URL = "https://crudcrud.com/api/53fa4cd709b84cd0bf4b5bdea7442835/appointments"
 
 // RESOURCE = "/appointment"
 
@@ -35,7 +35,7 @@ function saveuser(e) {
     e.preventDefault()
     if (inusername.value != "" || inemail.value != "") {
         data = JSON.parse(localStorage.getItem("data"))
-        
+
         if (data == null) {
             data = []
         }
@@ -49,21 +49,42 @@ function saveuser(e) {
         localStorage.setItem("data", data)
     }
     saveToCloud(datavalue)
-    let li = document.createElement("li")
-    li.className = "item"
-    li.appendChild(document.createTextNode(datavalue["user"]))
-    listofUsers.appendChild(li)
+    let div = document.createElement("div")
+    div.style.display = "flex"
+    let li_user = document.createElement("li")
+    li_user.className = "item"
+    li_user.id = "user"
+    li_user.appendChild(document.createTextNode(inusername.value))
+    console.log(li_user)
+    let li_email = document.createElement("li")
+    li_email.className = "item"
+    li_email.appendChild(document.createTextNode(inemail.value))
+
+    let btn_delete = document.createElement("button")
+    btn_delete.innerText = "Delete"
+    btn_delete.id = "delete"
+    btn_delete.addEventListener("click", delete_data)
+
+
+
+    div.appendChild(li_user)
+    div.appendChild(li_email)
+    div.appendChild(btn_delete)
+    listofUsers.appendChild(div)
 
     inusername.value = ""
     inemail.value = ""
 
 }
 
+
+var tempdata = {}
+
 window.addEventListener("DOMContentLoaded", () => {
-    axios.get("https://crudcrud.com/api/69f61b9dcb674b8893e403b3374bedaa/appointments")
+    axios.get("https://crudcrud.com/api/53fa4cd709b84cd0bf4b5bdea7442835/appointments")
 
         .then((response) => {
-            
+            console.log(response)
             tempdata = response.data
             keys = Object.keys(tempdata)
             keys.forEach((key) => {
@@ -71,26 +92,62 @@ window.addEventListener("DOMContentLoaded", () => {
                 div.style.display = "flex"
                 let li_user = document.createElement("li")
                 li_user.className = "item"
+                li_user.id = "user"
                 li_user.appendChild(document.createTextNode(tempdata[key].user))
                 console.log(li_user)
                 let li_email = document.createElement("li")
                 li_email.className = "item"
                 li_email.appendChild(document.createTextNode(tempdata[key].email))
+
+                let btn_delete = document.createElement("button")
+                btn_delete.innerText = "Delete"
+                btn_delete.id = "delete"
+                btn_delete.addEventListener("click", delete_data)
+
+
+
                 div.appendChild(li_user)
                 div.appendChild(li_email)
-                console.log(div)
+                div.appendChild(btn_delete)
+
 
                 listofUsers.appendChild(div)
             });
         }).catch((err) => {
-            console.log("Error occured"+err)
+            console.log("Error occured" + err)
         })
 })
 
 
+function delete_data(event) {
+    if (event.target.id == "delete") {
+        // console.log(event.target.parentNode)
+        div = event.target.parentNode
+
+
+        user = div.childNodes[0].innerText
+        let id = null
+        tempdata.forEach((data) => {
+            console.log(data)
+            if (data["user"] == user) {
+                console.log(data["user"])
+                id = data["_id"]
+            }
+        })
+        console.log(id)
+        axios.delete(`https://crudcrud.com/api/53fa4cd709b84cd0bf4b5bdea7442835/appointments/${id}`).then().catch(() => {
+            console.log("cannot delete element")
+        })
+
+        listofUsers.removeChild(div)
+    }
+
+
+}
+
 function saveToCloud(datavalue) {
     console.log("save data 1")
-    axios.post("https://crudcrud.com/api/69f61b9dcb674b8893e403b3374bedaa/appointments", datavalue)
+    axios.post("https://crudcrud.com/api/53fa4cd709b84cd0bf4b5bdea7442835/appointments", datavalue)
         .then(function (response) {
             console.log(response);
         })
